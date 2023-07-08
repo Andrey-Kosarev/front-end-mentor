098809a00ee476a0f3a3e73f913b7c7eedb0978e
akosarev <kosarev_andrey@mail.ru>
2023-07-08 19:23:44 +0200

    Newsletter signup

---
+++ 005_newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,8 @@
+// const form = document.querySelector('.subscription-form');
+
+// form.addEventListener('submit', (event)=>{
+//     event.preventDefault()
+//     let emailInput = event.target[0].validity
+
+//     console.log(emailInput)
+// })
